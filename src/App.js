ca3ce2c10dbfaf098e33055479902e9226f3c9dd
Aleksey <9802450@mail.ru>
2023-09-13 12:19:35 +0300

    first add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Image from "./components/image";
+import logo from "./img/logo.png";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      helpText: "Введите логин",
+      userData: "",
+    };
+    this.inputClick = this / this.inputClick.bind(this);
+  }
+  render() {
+    return (
+      <div>
+        <Header title="Страница регистрации" name="" />
+        <h1>Войдите или зарегистрируйтесь</h1>
+        <h2>{this.state.helpText}</h2>
+        <input
+          placeholder={this.state.helpText}
+          onClick={this.inputClick}
+          onMouseEnter={this.mouseOver}
+        />
+        <input placeholder="Введите пароль" />
+        <p>Логотип</p>
+        <Image image={logo} />
+      </div>
+    );
+  }
+
+  inputClick() {
+    this.setState({ helpText: "Ввод данных" });
+    console.log("Clicked");
+  }
+  mouseOver() {
+    console.log("Mouse Over");
+  }
 }
-
 export default App;
