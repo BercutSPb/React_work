c6d46a124b841dec947709daefb38b3f397f6945
Aleksey <9802450@mail.ru>
2023-10-01 21:16:32 +0300

    valid

--- src/App.js
+++ src/App.js
@@ -1,41 +1,17 @@
 import React from "react";
 import Header from "./components/Header";
-import Image from "./components/image";
-import logo from "./img/logo.png";
+import LoginForm from "./components/LoginForm";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      helpText: "Введите логин",
-      userData: "",
-    };
-    this.inputClick = this / this.inputClick.bind(this);
-  }
   render() {
     return (
       <div>
-        <Header title="Страница регистрации" name="" />
-        <h1>Войдите или зарегистрируйтесь</h1>
-        <h2>{this.state.helpText}</h2>
-        <input
-          placeholder={this.state.helpText}
-          onClick={this.inputClick}
-          onMouseEnter={this.mouseOver}
-        />
-        <input placeholder="Введите пароль" />
-        <p>Логотип</p>
-        <Image image={logo} />
+        <Header title="Страница регистрации" />
+        <div>
+          <LoginForm />
+        </div>
       </div>
     );
-  }
-
-  inputClick() {
-    this.setState({ helpText: "Ввод данных" });
-    console.log("Clicked");
-  }
-  mouseOver() {
-    console.log("Mouse Over");
   }
 }
 export default App;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,73 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+const LoginForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    getValues,
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label>Email:</label>
+        <input
+          type="email"
+          {...register("email", {
+            required: true,
+            pattern: { value: /^\S+@\S+$/i, message: "Email не правельный" },
+            validate: (value) => !value.includes(" ") || "Без пробелов",
+          })}
+        />
+        {errors.email && (
+          <span>
+            Требуется указать адрес электронной почты, который должен быть
+            действительным
+          </span>
+        )}
+      </div>
+      <div>
+        <label>Пароль:</label>
+        <input
+          type="password"
+          {...register("password", {
+            required: true,
+            minLength: {
+              value: 5,
+              message: "Минимальное количество символов 5",
+            },
+            validate: (value) => !value.includes(" ") || "без пробелов",
+          })}
+        />
+        {errors.password && (
+          <span>Пароль обязателен и должен содержать не менее 5 символов</span>
+        )}
+      </div>
+      <div>
+        <label>Подтвердите пароль:</label>
+        <input
+          type="password"
+          {...register("confirmPassword", {
+            required: true,
+            validate: {
+              matches: (value) => value === getValues("password"),
+              noSpaces: (value) => !value.includes(" ") || "Без пробелов",
+            },
+          })}
+        />
+        {errors.confirmPassword && (
+          <span>Пароли не совпадают или присутствуют пробелы</span>
+        )}
+      </div>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default LoginForm;
