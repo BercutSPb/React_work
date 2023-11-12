07bb546ed20fa7af60fc0e3ee6ecaae43ef19f1e
Алексей <9802450@mail.ru>
2023-11-12 16:22:18 +0300

    Перевод проекта на TypeScript

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,7 @@ import * as ReactDOMClient from "react-dom/client";
 import App from "./App";
 import "./css/main.css";
 
-const app = ReactDOMClient.createRoot(document.getElementById("app"));
-app.render(<App />);
+const app = ReactDOMClient.createRoot(document.getElementById("app") as HTMLElement)
+app.render(
+    <App />
+);
